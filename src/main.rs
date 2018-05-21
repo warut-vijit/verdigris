ec9c7b347757f86c160dc1d0d9f721e897c57d65
warut-vijit <warut.vijitbenjaronk@gmail.com>
2018-05-21 01:39:23 -0500

    parsing csv to ndarray, rnn step function

--- src/main.rs
+++ src/main.rs
@@ -6,39 +6,60 @@ use std::env;
 use std::io::Read;
 use std::fs::File;
 use std::sync::mpsc::channel;
-use nd::{Array1, Array2, ArrayView1};
+use nd::{Array1, Array2, ArrayView1, Dim};
 
-//let squash  |x: f32| -> f32 { 1 / (powf(-1. * x) + 1) };
+static NUM_STATE : usize = 61;
+static NUM_INPUT : usize = 41;
 
-fn read_csv() -> () {
-    let file_path = env::args_os().nth(1).unwrap();
-    println!("File path: {:?}", file_path);
+// Accepts a filename and a dimension, and outputs a 2D ndarray
+fn read_csv(filename : &String, dim : Dim<[usize; 2]>) -> Array2<f32> {
+    println!("File path: {:?}", *filename);
 
     // Read file into string
-    let mut file = File::open(file_path).unwrap();
+    let mut file = File::open(filename).unwrap();
     let mut buffer = String::new();
-    file.read_to_string(&mut buffer);
+    let _res = file.read_to_string(&mut buffer);
 
     // Split string into array of floats
-    let mut str_rows: Vec<&str> = buffer.split('\n').collect();
-    if str_rows.last().cloned().expect("") == "" {
-        str_rows.pop();
+    let mut str_mat: Vec<&str> = buffer.split(|c| (c=='\n') || (c==',')).collect();
+    if str_mat.last().cloned().expect("") == "" { // remove last blank line
+        str_mat.pop();
     }
-    let str_mat: Vec<Vec<&str>> = str_rows.iter().map(|r| r.split(',').collect()).collect();
-    //let f32_mat: Vec<Vec<Result<f32,<f32 as std::str::FromStr>::Err>>> = str_mat.iter().map(|r| r.iter().map(|c| c.parse::<f32>()).collect()).collect();
-    let f32_mat: Vec<Vec<f32>> = str_mat.iter().map(|r| r.iter().map(|c| c.parse::<f32>().unwrap()).collect()).collect();
-    
-    for row in f32_mat {
-        println!(" |  {:?}", row);
-    }
+    let f32_mat: Vec<f32> = str_mat.iter().map(|c| c.parse::<f32>().unwrap()).collect();
+    Array2::from_shape_vec(dim, f32_mat).unwrap()
+}
+
+// Takes a mutable reference to an array, and applies elementwise sigmoid
+fn squash(v : &Array1<f32>) -> Array1<f32> {
+    let sq_clos = |x: f32| -> f32 { 1. / ((-1. * x).exp() + 1.) };
+    v.mapv(sq_clos)
+}
+
+// Applies one RNN iteration to a 1D ndarray
+fn rnn_step(m : &Array2<f32>, v : &Array1<f32>) -> Array1<f32> {
+    let lin = m.dot(v);
+    squash(&lin)
+}
+fn rnn_forward(m : &Array2<f32>, its : u32) -> Array1<f32> {
+    // TODO: next
 }
 
 fn main() {
-    read_csv();
+
+    // receive command line arguments
+    let cmd : Vec<String> = env::args().collect();
+
+    let weight_mat_dim = Dim([NUM_STATE, NUM_INPUT + NUM_STATE]);
+    let weight_mat : Array2<f32> = read_csv(&cmd[1], weight_mat_dim);
+    println!("weight matrix: {}", weight_mat);
+
+    let a = Array2::from_elem((5, 4), 3.);
     let b = Array1::from_elem(4, 2.);
-    let mut a = Array2::from_elem((5, 4), 3.);
-    a.mapv_inplace(|x| x + 1.);
+    let b = squash(&b);
     let c : Array1<f32> = a.dot(&b);
+    let c = squash(&c);
+    let bcopy = rnn_step(&a, &b);
+    assert!(bcopy == c);
     let d = Array2::from_shape_vec((2, 2), vec![1., 2., 3., 4.]).unwrap();
     println!("{:?}, {:?}", a, d);
     let e : ArrayView1<f32> = d.slice(s![0, ..]);
